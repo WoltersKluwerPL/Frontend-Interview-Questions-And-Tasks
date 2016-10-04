6b45b87caa5ac2159d84cf1ae529493807789ee1
mrzepinski <Maciej.Rzepinski@wolterskluwer.com>
2016-10-04 07:19:12 +0200

    Intellij fixes for variable scopes

--- tasks/javascript/variable-scope-1.js
+++ tasks/javascript/variable-scope-1.js
@@ -2,48 +2,48 @@
  * TODO: What will be the output of the following snippet?
  */
 
-var n = 1;
+var n1 = 1;
 
-(function printSomething () {
-  console.log(n);
-  var n = 2;
-  console.log(n);
+(function printSmth () {
+  console.log(n1);
+  var n1 = 2;
+  console.log(n1);
 })();
 
-console.log(n);
+console.log(n1);
 
 //
 
-n = 1;
+n2 = 1;
 
-(function printSomething () {
-  console.log(n);
-  var n = 2;
-  console.log(n);
+(function printSmth () {
+  console.log(n2);
+  var n2 = 2;
+  console.log(n2);
 })();
 
-console.log(n);
-
-// 
-
-var n = 1;
-
-(function printSomething () {
-  console.log(n);
-  n = 2;
-  console.log(n);
-})();
-
-console.log(n);
+console.log(n2);
 
 //
 
-n = 1;
+var n3 = 1;
 
-(function printSomething () {
-  console.log(n);
-  n = 2;
-  console.log(n);
+(function printSmth () {
+  console.log(n3);
+  n3 = 2;
+  console.log(n3);
 })();
 
-console.log(n);
+console.log(n3);
+
+//
+
+n4 = 1;
+
+(function printSmth () {
+  console.log(n4);
+  n4 = 2;
+  console.log(n4);
+})();
+
+console.log(n4);
